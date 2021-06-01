f3af15aaf6b0ea2cc686e9d0aea8097c2ddc6355
Mohammed Zaheen <mzaheen610@gmail.com>
2021-06-01 15:10:23 +0530

    Corrections were made

--- estimate.py
+++ estimate.py
@@ -3,19 +3,18 @@ import unittest
 
 def wallis(num):
   pi = 1
-  for x in range(1,num):
+  for x in range(1,num+1):
       pi=pi*((4*x**2)/((4*x**2)-1))
-  print("Value of pi is:",pi*2)
   return pi*2
 
 
 def monte_carlo(darts):
   point_circle=0
-  for y in range(1,darts):
+  for y in range(darts):
      point_x=random.random()
      point_y=random.random()
      d=math.sqrt(point_x**2+point_y**2)
-     if d<=1:
+     if d<1:
         point_circle=point_circle+1
   pi=4*(point_circle/darts)
   return pi 
